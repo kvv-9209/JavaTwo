56fcff5a5c3a17ccbf749c2f73fef9805573841a
Valentin <kvv-9209@mail.ru>
2021-10-11 15:42:01 +0300

    Home work 1

--- src/main/java/lesson1/HomeWork1/Participants.java
+++ src/main/java/lesson1/HomeWork1/Participants.java
@@ -1,14 +1,6 @@
 package lesson1.HomeWork1;
 
-public interface Participants {
-
-    default void jump() {
-        System.out.println("перепрыгнул");
-    }
-
-    default void run() {
-        System.out.println("пробежал");
-    }
+public interface Participants extends Jumpable, Runable{
 
     boolean obstacle(Obstacle obstacle);
 
